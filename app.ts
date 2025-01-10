d8f90a07700ea853e29c0ac0d42b7a9443f6173a
ARIKBOGA <barikboga42@gmail.com>
2025-01-11 00:43:05 +0300

    Implemented Union Types

--- app.ts
+++ app.ts
@@ -1,50 +1,29 @@
-// const person: {
-//   name: string;
-//   nickname: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   name: "Burak",
-//   nickname: "CodeCaptain",
-//   age: 35,
-//   hobbies: ["Coding", "Cooking", "Learning"],
-//   role: [2, "author"],
-// };
-
-enum Role { ADMIN = 'ADMIN', AUTHOR = 100, READ_ONLY = 0}
-
-const person = {
-  name: "Burak",
-  nickname: "CodeCaptain",
-  age: 35,
-  hobbies: ["Coding", "Cooking", "Learning"],
-  role: Role.ADMIN,
-};
-
-let favoriteActivities: string[];
-favoriteActivities = ["Sports", "Swimming", "Fishing"];
-
-console.log("Name: " + person.name);
-console.log("Age: " + person.age);
-console.log("Nickname: " + person.nickname);
-
-person.hobbies.forEach((hobby) => {
-  console.log("Hobby: " + hobby.toUpperCase());
-});
-
-person.hobbies.push(...favoriteActivities);
-
-person.hobbies.forEach((hobby) => {
-  console.log("Hobby: " + hobby.toUpperCase());
-});
-
-//person.role.push("admin"); // push operation is a exception. Need to be avoided for tuples cuz typescript can not detect it
-//person.role[1] = 10; // this error will be thrown by typescript
-
-if(person.role === Role.ADMIN) {
-    console.log("is admin");
-}else {
-    console.log("is not admin");
+/**
+ * Combines two values of type number or string into a single value.
+ * If both values are numbers, the result is the sum of the two numbers.
+ * If one or both values are strings, the result is a concatenation of the two strings.
+ * @param {number|string} arg1 The first value to combine.
+ * @param {number|string} arg2 The second value to combine.
+ * @returns {number|string} The combined value.
+ */
+function combine(arg1: number | string, arg2: number | string, resultConversion: "as-number" | "as-text"): number | string {
+  let result: any;
+  if (typeof arg1 === "number" && typeof arg2 === "number" || resultConversion === 'as-number') {
+    result = +arg1 + +arg2;
+  } else {
+    result = arg1.toString() + arg2.toString();
+  }
+  return result;
 }
-console.log(person.role);
+
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+
+const combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
+
+const combinedNamesAndAges = combine("Max", 26, "as-text");
+console.log(combinedNamesAndAges);
+
+const combinedAgesAsString = combine("30", "26", "as-number");
+console.log(combinedAgesAsString);
