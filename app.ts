d2be460f162ae7b992c3275320be6f402e38c881
ARIKBOGA <barikboga42@gmail.com>
2025-01-10 14:57:35 +0300

    Implemented Typescript Basics, Arrays, Objects, Tuples types

---
+++ app.ts
@@ -0,0 +1,36 @@
+const person: {
+  name: string;
+  nickname: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "Burak",
+  nickname: "CodeCaptain",
+  age: 35,
+  hobbies: ["Coding", "Cooking", "Learning"],
+  role: [2, "author"],
+};
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports", "Swimming", "Fishing"];
+
+console.log("Name: " + person.name);
+console.log("Age: " + person.age);
+console.log("Nickname: " + person.nickname);
+
+person.hobbies.forEach((hobby) => {
+  console.log("Hobby: " + hobby.toUpperCase());
+});
+
+person.hobbies.push(...favoriteActivities);
+
+person.hobbies.forEach((hobby) => {
+  console.log("Hobby: " + hobby.toUpperCase());
+});
+
+
+person.role.push("admin"); // push operation is a exception. Need to be avoided for tuples cuz typescript can not detect it
+//person.role[1] = 10; // this error will be thrown by typescript
+
+console.log(person.role);
